70907ee0c1acf7d176bb13e984fd309b2b51b0e0
Jeremy Webb UofT <webbjere@Jeremys-MacBook-Pro.local>
2020-01-07 11:05:58 -0500

    Updating documentation

--- nbodypy/main/cluster.py
+++ nbodypy/main/cluster.py
@@ -106,6 +106,109 @@ class StarCluster(object):
     def __init__(
         self, ntot=0, tphys=0.0, units=None, origin=None, ctype="snapshot", **kwargs
     ):
+    r"""A class that represents a star cluster population that functions can be performed on
+
+    The StarCluster class, which represents a population of star particles, that all nbodpy
+    functions are written to act on. Functions can be called internally (which results in new class
+    variables beings set) or externally (where calculated variables are outputs). Measurements
+    of variable profiles (i.e. density profile) can only be calculated externally.
+
+    To declare a StarCluster, no initial variables are necessary and stars can be added 
+    separately. It is, however, important to set 'units' and 'origin' if any unit conversion
+    or changes of origin are going to be performed
+
+    Parameters
+    ----------
+    ntot : int
+        Number of stars in the cluster (default: 0)
+    tphys : float
+        Time (units not necessary) associated with the population (default: 0)
+    units : str
+        Units of stellar positions and velocties. Options include 'realpc',
+        'realkpc','radec','nbody',and 'galpy'. For 'realpc' and 'realkpc', 
+        stellar velocities are assumed to be km/s. (default: None)
+    origin : str
+        Origin of coordinate system within which stellar positions and velocities are defined. 
+        Options include 'centre', 'cluster', 'galaxy' and 'sky'. Note that 'centre' corresponds
+        to the systems centre of density or mass (as calculated by nbodypy) while 'cluster' corresponds
+        to the orbital position of the cluster's initial centre of mass given 'tphys'. In some cases
+        these two coordinate systems may be the same. (default: None)
+    ctype : str
+        Code type used to generate the cluster population. Current options include 'snapshot',
+        'nbody6','nbody6se','gyrfalcon','snaptrim', amuse','nbodypy','snapauto'. The parameter
+        informs nbodypy how to load the stellar popuation and advance to the next snapshot.
+        (default: 'snapshot')
+    **kwargs
+
+    long_var_name : {'hi', 'ho'}, optional
+        Choices in brackets, default first when optional.
+
+    Returns
+    -------
+    type
+        Explanation of anonymous return value of type ``type``.
+    describe : type
+        Explanation of return value named `describe`.
+    out : type
+        Explanation of `out`.
+    type_without_description
+
+    Other Parameters
+    ----------------
+    only_seldom_used_keywords : type
+        Explanation
+    common_parameters_listed_above : type
+        Explanation
+
+    Raises
+    ------
+    BadException
+        Because you shouldn't have done that.
+
+    See Also
+    --------
+    numpy.array : Relationship (optional).
+    numpy.ndarray : Relationship (optional), which could be fairly long, in
+                    which case the line wraps here.
+    numpy.dot, numpy.linalg.norm, numpy.eye
+
+    Notes
+    -----
+    Notes about the implementation algorithm (if needed).
+
+    This can have multiple paragraphs.
+
+    You may include some math:
+
+    .. math:: X(e^{j\omega } ) = x(n)e^{ - j\omega n}
+
+    And even use a Greek symbol like :math:`\omega` inline.
+
+    References
+    ----------
+    Cite the relevant literature, e.g. [1]_.  You may also cite these
+    references in the notes section above.
+
+    .. [1] O. McNoleg, "The integration of GIS, remote sensing,
+       expert systems and adaptive co-kriging for environmental habitat
+       modelling of the Highland Haggis using object-oriented, fuzzy-logic
+       and neural-network techniques," Computers & Geosciences, vol. 22,
+       pp. 585-588, 1996.
+
+    Examples
+    --------
+    These are written in doctest format, and should illustrate how to
+    use the function.
+
+    >>> a = [1, 2, 3]
+    >>> print([x + 3 for x in a])
+    [4, 5, 6]
+    >>> print("a\nb")
+    a
+    b
+    """
+
+
         """
         NAME:
 

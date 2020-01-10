Fixed error in rmpro calculation

nbodypy/main/cluster.py
@@ -1229,16 +1229,16 @@ class StarCluster(object):
         if self.rorder is not None:
             msum = np.cumsum(self.m[self.rorder])
             indx = msum >= 0.5 * self.mtot
-            self.rm = self.r[self.rorder[indx][0]] #Moved [0] inside: TODO to all
+            self.rm = self.r[self.rorder[indx][0]]  
             indx = msum >= 0.1 * self.mtot
             self.r10 = self.r[self.rorder[indx][0]]
 
         if self.projected and self.rproorder is not None:
             msum = np.cumsum(self.m[self.rproorder])
             indx = msum >= 0.5 * self.mtot
-            self.rmpro = self.r[self.rproorder[indx][0]]
+            self.rmpro = self.rpro[self.rproorder[indx][0]]
             indx = msum >= 0.1 * self.mtot
-            self.r10pro = self.r[self.rproorder[indx][0]]
+            self.r10pro = self.rpro[self.rproorder[indx][0]]
         else:
             self.rmpro = 0.0
             self.r10pro = 0.0
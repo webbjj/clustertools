c6270e56ad8fdcc00b7a1497f5a45a94d24b9387
webbjj <webb.jjw@gmail.com>
2020-06-22 14:46:33 -0400

    still fixing docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,8 +35,7 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['nbsphinx','sphinx.ext.todo', "nbsphinx", 'sphinx.ext.viewcode', 'sphinx.ext.autodoc',
-'sphinxcontrib.bibtex','sphinx-astropy',
+extensions = ['nbsphinx','sphinx.ext.todo', "nbsphinx", 'sphinx.ext.viewcode', 'sphinx.ext.autodoc','sphinx-astropy',
 'sphinx.ext.autosectionlabel','sphinx.ext.mathjax','sphinx.ext.ifconfig','sphinx_automodapi.smart_resolver',
 ]
 

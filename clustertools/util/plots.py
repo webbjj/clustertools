7833f074acae00d370bece15d2a5d6cdafee8d97
webbjj <webb.jjw@gmail.com>
2021-05-19 11:48:42 -0400

    m0 fix

--- clustertools/util/plots.py
+++ clustertools/util/plots.py
@@ -15,7 +15,7 @@ import os
 from scipy.ndimage import gaussian_filter
 import matplotlib.colors as colors
 
-gplot.print(axes_labelsize=18.0, xtick_labelsize=14.0, ytick_labelsize=14.0)
+gplot.start_print(axes_labelsize=18.0, xtick_labelsize=14.0, ytick_labelsize=14.0)
 current_palette = sns.color_palette()
 
 def _scatter(

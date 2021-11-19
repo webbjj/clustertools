6ad14b2d84d0fbd3e6c4da57c1ad8bef1aeae511
webbjj <webb.jjw@gmail.com>
2021-11-19 15:53:32 -0500

    update calculation of tbar to match Nbody6

--- clustertools/cluster/cluster.py
+++ clustertools/cluster/cluster.py
@@ -661,6 +661,13 @@ class StarCluster(object):
         ----------
         2018 - Written - Webb (UofT)
         """
+
+
+        #Define GM, AU and PC in cgs units (2009 IAU values) - Taken from Nbody6
+        GM = 1.32712442099e26
+        AU = 1.49597870700e13
+        PC = 1296000.0/(2.0*np.pi)*AU
+
         # Number of stars in the core
         self.nc = nc
         # Core radius
@@ -678,7 +685,12 @@ class StarCluster(object):
         # Velocity scaling parameter
         self.vbar = vbar
         # Time scaling parameter
-        self.tbar = self.rbar/self.vbar
+        #Form time scale in seconds and velocity scale in km/sec.
+        self.tbar=np.sqrt(PC/GM)*PC
+        #Convert time scale from units of seconds to million years.
+        self.tbar/=(3.15576e07*1.0e06)
+        #Scale to working units of RBAR in pc & ZMBAR in solar masses.
+        self.tbar*=np.sqrt((self.rbar**3.)/(self.zmbar**2.))
         # Scale radius of cluster
         self.rscale = rscale
         # Number of single stars

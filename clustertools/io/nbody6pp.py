a526f4dbbda0b1ccf6d885b71fa38acf4f68d6e8
webbjj <webb.jjw@gmail.com>
2022-01-14 14:38:25 -0500

    debug reading binary values in nb6pp

--- clustertools/io/nbody6pp.py
+++ clustertools/io/nbody6pp.py
@@ -106,7 +106,7 @@ def _get_nbody6pp(conf3, bev82=None, sev83=None, snap40=None, ofile=None, advanc
             pb=(10.0**np.array(pb))/cluster.tbar_days
             semi=(10.0**np.array(semi))/cluster.rbar_su
             m1b=np.array(m1b)/cluster.zmbar
-            m2b=np,array(m2b)/cluster.zmbar
+            m2b=np.array(m2b)/cluster.zmbar
 
             cluster.add_bse(i_d1,i_d2,kw1,kw2,kwb,ecc,pb,semi,m1b,m2b,zl1b,zl2b,r1b,r2b,ep1=te1,ep2=te2)
             cluster.add_sse(kw1,lbtot,np.maximum(r1b,r2b))

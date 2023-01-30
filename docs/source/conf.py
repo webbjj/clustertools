6c0037c956e44c127d88d1637e483e4e0cac1bdd
webbjj <webb.jjw@gmail.com>
2023-01-30 14:30:47 -0500

    1.0.10

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ else:
 author = 'Jeremy J. Webb'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.9'
+release = '1.0.10'
 
 
 # -- General configuration ---------------------------------------------------
